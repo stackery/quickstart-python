61e7043e09bbac0d404cac333ede9b09fe9eb433
Anna Spysz <anna.spysz@stackery.io>
2019-06-26 16:01:51 -0700

    Added function code

--- src/getItems/handler.py
+++ src/getItems/handler.py
@@ -1,6 +1,26 @@
-import json
-def handler(event, context):
-    # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
+import os
+import boto3
 
-    return {}
+dynamodb = boto3.resource('dynamodb')
+
+def handler(event, context):
+  table_name = os.environ['TABLE_NAME'] # get the table name from the automatically populated environment variables
+  table = dynamodb.Table(table_name)
+
+  # Use dynamodb to get items from the ItemTable
+  result = table.scan()
+  result_count = result['Count']
+  items = result['Items']
+
+  for item in items:
+    item_id = item['id']
+    content = item['content']
+    print(f'Item {item_id}: {content}')
+
+  # Create a response
+  response = {
+    'statusCode': 200,
+    'body': f'{result_count} items found'
+  }
+
+  return response
--- src/postItem/handler.py
+++ src/postItem/handler.py
@@ -1,6 +1,27 @@
-import json
-def handler(event, context):
-    # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
+import os
+import boto3
 
-    return {}
+dynamodb = boto3.resource('dynamodb')
+
+def handler(event, context):
+  table_name = os.environ['TABLE_NAME'] # get the table name from the automatically populated environment variables
+  table = dynamodb.Table(table_name)
+
+  params = {
+    'id': '5', # modify with each invoke so the id does not repeat
+    'content': 'Whee' # modify content here
+  }
+
+  # Write a new item to the ItemTable
+  item_id = params.get('id')
+  print(f'Adding item {item_id} to table {table_name}')
+  table.put_item(Item = params, ConditionExpression='attribute_not_exists(id)') # do not overwrite existing entries
+  print('Item added to table, done')
+
+  # Return a 200 response if no errors
+  response = {
+    'statusCode': 200,
+    'body': 'Success!'
+  }
+
+  return response
